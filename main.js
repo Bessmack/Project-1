c446f763bde7e7fcc64724f4dcc32f7b2e8d315c
Bessmack <jenwied4lif@gmail.com>
2025-06-25 15:32:41 +0300

    Add functionality to the top navbar elements

---
+++ main.js
@@ -0,0 +1,23 @@
+document.querySelectorAll("#ul2 li").forEach(li => {
+  li.addEventListener('mousedown', function(){
+    const id = li.textContent.toLowerCase();
+    const section = document.querySelector(`#${id}Section`);
+    section.scrollIntoView({behavior:"smooth"});
+  });
+});
+
+document.querySelector("#about").addEventListener('mousedown', function(){
+    const footer = document.getElementById("footerSection");
+    footer.scrollIntoView({behavior:"smooth"});
+    }
+)
+document.querySelector(".cat").addEventListener('mousedown', function(){
+    const category = document.getElementById("navbar2");
+    category.scrollIntoView({behavior:"smooth"});
+    }
+)
+document.querySelector(".cat").addEventListener('mousedown', function(){
+    const category = document.getElementById("navbar2");
+    category.scrollIntoView({behavior:"smooth"});
+    }
+)
