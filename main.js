5c6b0556080b87c2d76ab0195d5656cf1b4e2956
Bessmack <jenwied4lif@gmail.com>
2025-06-25 21:40:48 +0300

    Add items from an API through a search input

--- main.js
+++ main.js
@@ -28,3 +28,89 @@ document.querySelector("#home").addEventListener("mousedown", ()=>{
     let homePage = document.getElementById("topNav");
     homePage.scrollIntoView({behavior:"smooth"});
 })
+
+
+
+
+
+// Beginning of My TRUE Code
+
+const baseUrl = "https://makeup-api.herokuapp.com/api/v1/products.json?"
+const form = document.getElementById('form1');
+
+
+
+form.addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let searchValue = document.getElementById("inpSearch").value.toLowerCase();
+    fetch(`${baseUrl}brand=${searchValue}`)
+    .then(res => res.json())
+    .then(data => {
+        data.forEach(product => {
+            renderProduct(product);
+            renderItem(product);
+        })
+    });
+    form.reset();
+});
+
+function renderItem(product){
+    const name = product.name;
+    const description = product.description;
+    const currency = product.currency;
+    const price = product.price;
+    const image = product.image_link;
+
+    let searchSection = document.getElementById('fourthSection');
+    let div = document.createElement('div');
+    div.innerHTML = `
+        <button class="imgContainer2" style="background-image: url('${image}'); background-size: cover;"></button>
+        <h3>${name}</h3>
+        <p>${description || "No description available."}</p>
+        <p>${currency || "No Currency"}: ${price || "FREE"}</p>
+        <button class="add2">ADD</button>`;
+
+    searchSection.appendChild(div);
+};
+
+
+
+document.getElementById('form2').addEventListener('submit', (e)=>{
+    e.preventDefault();
+    let itemName = document.getElementById('productName').value;
+
+    console.log(itemName);
+});
+function renderProduct(product){
+    const brand = product.brand;
+    const name = product.name;
+    const categ = product.category;
+    const description = product.description;
+    const currency = product.currency;
+    const price = product.price;
+    const colors = product.colors;
+    const image = product.image_link;
+
+    console.log(brand);
+    console.log(name);
+    console.log(categ);
+    console.log(description);
+    console.log(currency);
+    console.log(price);
+    console.log(colors);
+    console.log(image);
+};
+
+
+// fetch(baseUrl)
+// .then(res => res.json())
+// .then(data => {
+//     data.forEach(product => renderProduct(product))
+// })
+
+
+// https://www.affirmations.dev/
+// https://makeup-api.herokuapp.com/api/v1/products.json?
+// ONE PRODUCT COMPANY = https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline
+
+
